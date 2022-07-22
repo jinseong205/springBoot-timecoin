2b3d73006d3f9a015eef7346d903231f9801db01
jinseong <jinseong205@gmail.com>
2022-07-23 01:43:46 +0900

    Initial commit

---
+++ timecoin/src/main/java/com/jinseong/timecoin/TimecoinApplication.java
@@ -0,0 +1,13 @@
+package com.jinseong.timecoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimecoinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimecoinApplication.class, args);
+	}
+
+}
---
+++ timecoin/src/test/java/com/jinseong/timecoin/TimecoinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinseong.timecoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimecoinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
