6de68b53ce365ada1ecfc9a5ed937795509f2b7e
jinseong <jinseong205@gmail.com>
2022-07-24 19:07:26 +0900

    Update App.js

--- timecoin/src/main/frontend/src/App.js
+++ timecoin/src/main/frontend/src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+1.실행방식
+>[package.json] scripts - start : react-scripts start 
+>[index.js] 순차 실행 > ReactDom.render(...<App>,document.getElementById('root')...) ++ jsx
+>[App.js] 을 [public/index.html -> <div id = "root">] 에 render
+
+2. JSX 문법
+*/
 function App() {
   return <div>안녕1</div>;
 }
