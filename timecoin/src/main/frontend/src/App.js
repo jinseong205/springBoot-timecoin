20e2ccd1221a98f8c7edb568cceb1b04147d09ad
jinseong <jinseong205@gmail.com>
2022-07-28 19:52:54 +0900

    ^^7

--- timecoin/src/main/frontend/src/App.js
+++ timecoin/src/main/frontend/src/App.js
@@ -1,8 +1,32 @@
-import { useState } from 'react';
+import { createRef, useRef, useState } from 'react';
 import './App.css';
 
+//useRef (디자인)
+//dom 을 변경할 때 사용
 function App() {
-  return <div>1</div>;
+  const myRef = useRef(null);
+  const [list, setList] = useState([
+    { id: 1, name: '진성' },
+    { id: 2, name: '푸른' },
+  ]);
+  const myRefs = Array.from({ length: list.length }).map(() => createRef());
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          console.log(myRef);
+          myRefs[1].current.style.backgroundColor = 'red';
+        }}
+      >
+        색 변경
+      </button>
+      <div ref={myRef}>박스</div>
+      {list.map((user, index) => (
+        <h1 ref={myRefs[index]}>{user.name}</h1>
+      ))}
+    </div>
+  );
 }
 
 export default App;
