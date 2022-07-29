42aacdf5dd58871b8682b33d1ff0c890f70fa61f
jinseong <jinseong205@gmail.com>
2022-07-29 14:24:47 +0900

    ^^7

--- timecoin/src/main/frontend/src/pages/HomePage.js
+++ timecoin/src/main/frontend/src/pages/HomePage.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
+import Home from '../components/home/Home';
 
 const HomePage = () => {
+  //http 요청 (fetch, axios (다운))
+  const [boards,setBoards] = useState([]);
   return (
     <div>
       <Header />
+      <Home/>
       <Footer />
     </div>
   );
