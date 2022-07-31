6d80901c301d37ae3aef3fd269fc4cf0718bd471
jinseong <jinseong205@gmail.com>
2022-07-31 20:57:50 +0900

    ^^7

--- timecoin/src/main/frontend/src/App.js
+++ timecoin/src/main/frontend/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Header />
+      <Footer />
     </div>
   );
 }
