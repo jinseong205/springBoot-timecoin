84a00f6758ad3427c1936ad3b70ed0eced82e489
jinseong <jinseong205@gmail.com>
2022-07-31 21:02:58 +0900

    ^^7

--- timecoin/src/main/frontend/src/App.js
+++ timecoin/src/main/frontend/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 
@@ -8,6 +9,8 @@ function App() {
   return (
     <div>
       <Header />
+      <Route path="/" exact={true} component={HomePage} />
+      <Route path="/login" exact={true} component={LoginPage} />
       <Footer />
     </div>
   );
